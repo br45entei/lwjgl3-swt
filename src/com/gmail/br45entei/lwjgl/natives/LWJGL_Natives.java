7267cbe6c2b9e1e2cb6bd65a62c403f6454468d7
Brian_Entei <br45entei@gmail.com>
2020-06-13 18:38:47 -0500

    Moved Platform and Architecture to org.eclipse.swt_x86_64 project
    - Removed Platform and Architecture classes (moved to
    org.eclipse.swt_x86_64)

--- src/com/gmail/br45entei/lwjgl/natives/LWJGL_Natives.java
+++ src/com/gmail/br45entei/lwjgl/natives/LWJGL_Natives.java
@@ -18,7 +18,9 @@
  *******************************************************************************/
 package com.gmail.br45entei.lwjgl.natives;
 
+import com.gmail.br45entei.util.Architecture;
 import com.gmail.br45entei.util.CodeUtil;
+import com.gmail.br45entei.util.Platform;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -93,70 +95,6 @@ public class LWJGL_Natives {
 				return null;
 			}
 		});
-	}
-	
-	/** This class contains modified code (as of 06/06/2020 05:47 PM CST) copied
-	 * from {@link org.lwjgl.system.Platform} because using any class from LWJGL
-	 * before the native libraries are loaded in causes an
-	 * {@link UnsatisfiedLinkError}, resulting in the libraries not being loaded
-	 * properly.
-	 * 
-	 * @author LWJGL3-3.2.3 (Ripped from Platform.java) */
-	public static enum Platform {
-		WINDOWS,
-		LINUX,
-		MACOSX,
-		UNKNOWN;
-		
-		private static final Platform current;
-		
-		static {
-			String osName = System.getProperty("os.name");
-			if(osName.startsWith("Windows")) {
-				current = WINDOWS;
-			} else if(osName.startsWith("Linux") || osName.startsWith("FreeBSD") || osName.startsWith("SunOS") || osName.startsWith("Unix")) {
-				current = LINUX;
-			} else if(osName.startsWith("Mac OS X") || osName.startsWith("Darwin")) {
-				current = MACOSX;
-			} else {
-				current = UNKNOWN;
-			}
-		}
-		
-		public static final Platform get() {
-			return current;
-		}
-		
-	}
-	
-	/** This class contains modified code (as of 06/06/2020 05:47 PM CST) copied
-	 * from {@link org.lwjgl.system.Platform.Architecture} because using any
-	 * class from LWJGL before the native libraries are loaded in causes an
-	 * {@link UnsatisfiedLinkError}, resulting in the libraries not being loaded
-	 * properly.
-	 * 
-	 * @author LWJGL3-3.2.3 (Ripped from Platform.java) */
-	public static enum Architecture {
-		X86,
-		X64,
-		ARM32,
-		ARM64;
-		
-		private static final Architecture current;
-		
-		static {
-			String osArch = System.getProperty("os.arch");
-			boolean is64Bit = osArch.contains("64") || osArch.startsWith("armv8");
-			
-			current = osArch.startsWith("arm") || osArch.startsWith("aarch64")//
-					? (is64Bit ? Architecture.ARM64 : Architecture.ARM32)//
-					: (is64Bit ? Architecture.X64 : Architecture.X86);
-		}
-		
-		public static final Architecture get() {
-			return current;
-		}
-		
 	}
 	
 	/** Loads the native libraries required by LWJGL3-3.2.3 along with any other
